d00a70a5dada89a7f863dfbefd692fa03f115d35
Ryugi62 <xorjf1027@naver.com>
2025-02-01 18:31:50 +0900

    feat (frontend) : 홈 페이지 컴포넌트 추가 및 소개 페이지 링크 제거

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 
+import Home from './pages/Home';
+
 function App() {
   return (
     <>
       <header>
         <nav>
           <Link to="/">홈</Link>
-          <Link to="/about">소개</Link>
         </nav>
       </header>
       <Routes>
-        <Route path="/" element={<h1>메인 페이지</h1>} />
-        <Route path="/about" element={<h1>소개 페이지</h1>} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </>
   );
